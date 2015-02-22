e3be035a94fb2a229c6d3ecb81d339ef32405209
Mariel Van Norman <melvinopolis@gmail.com>
2015-02-21 21:05:23 -0500

    Adds more pages and javascript navbar and footer

---
+++ js/footer.js
@@ -0,0 +1,11 @@
+document.write(
+	"<footer class='container-fluid'>" +
+		"<p>&copy; mariel van norman 2015</p>" +
+	            "<ul>" +
+	                "<li><a href='index.html'>home</a></li>" +
+	                "<li><a href='about.html'>about</a></li>" +
+	                "<li><a href='contact.html'>contact</a></li>" +
+	                "<li><a href='shops.html'>shops</a></li>" +
+	            "</ul>" +
+	"</footer>"
+	)
---
+++ js/navbar.js
@@ -0,0 +1,31 @@
+document.write(
+	"<nav class='navbar navbar-default navbar-fixed-top'>" +
+            "<div class='container-fluid'>" +
+	            "<div class='navbar-header'>" +
+	                "<button type='button' class='navbar-toggle collapsed' data-toggle='collapse' data-target='#bs-example-navbar-collapse-1'>" +
+	                    "<span class='sr-only'>Toggle navigation</span>" +
+	                    "<span class='icon-bar'></span>" +
+	                    "<span class='icon-bar'></span>" +
+	                    "<span class='icon-bar'></span>" +
+	                "</button>" +
+	                "<a class='navbar-brand' href='index.html'>" +
+	                    "<img alt='Brand' height='30px' src='images/m logo.png'>" +
+	                "</a>" +
+	            "</div>" +
+
+            "<div class='collapse navbar-collapse'>" +
+                "<ul class='nav navbar-nav'>" +
+                    "<li><a href='index.html'>home</a></li>" +
+                    "<li><a href='code.html'>code</a></li>" +
+                    "<li><a href='costumes.html'>costumes</a></li>" +
+                    "<li><a href='commissions.html'>commissions</a></li>" +
+                    "<li><a href='shops.html'>shops</a></li>" +
+                "</ul>" +
+                "<ul class='nav navbar-nav navbar-right'>" +
+                    "<li><a href='about.html'>about</a></li>" +
+                    "<li><a href='contact.html'>contact</a></li>" +
+                "</ul>" +
+            "</div><!-- /.navbar-collapse -->" +
+            "</div><!-- /.container-fluid -->" +
+        "</nav>"
+    )
